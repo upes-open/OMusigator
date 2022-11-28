31f80f24deb21e0f456ae9f48a6d013b7c0f8c4c
bhavika <bhavika.j.2502@gmail.com>
2022-11-29 01:45:59 +0530

    added frontend

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from "./components/Navbar";
+import Transfer from './components/Transfer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Transfer />
+    </div>
+  );
+}
+
+export default App;
